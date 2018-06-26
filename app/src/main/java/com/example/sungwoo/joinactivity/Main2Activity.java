b90520fcf364047f4b7fb924e3172b9466f0c1d5
sungwoo <parksw0824@naver.com>
2018-06-26 18:05:31 +0900

    layout

--- app/src/main/java/com/example/sungwoo/joinactivity/Main2Activity.java
+++ app/src/main/java/com/example/sungwoo/joinactivity/Main2Activity.java
@@ -7,36 +7,10 @@ import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
-    Button bt1,bt2,bt3;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        bt1 = (Button)findViewById(R.id.bt_1);
-        bt2 = (Button)findViewById(R.id.bt_2);
-        bt3 = (Button)findViewById(R.id.bt_3);
-
-        bt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        bt2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        bt3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
     }
 }
--- app/src/main/java/com/example/sungwoo/joinactivity/MainActivity.java
+++ app/src/main/java/com/example/sungwoo/joinactivity/MainActivity.java
@@ -10,39 +10,21 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText et_id, et_pw;
-    Button login_btn;
-
-    String input_id;
-    String input_password;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        input_id = "psw";
-        input_password = "psw";
-
-        et_id = (EditText) findViewById(R.id.et_Id);
-        et_pw = (EditText) findViewById(R.id.et_Password);
-        login_btn = (Button)findViewById(R.id.bt_Join);
-
-
-        login_btn.setOnClickListener(new View.OnClickListener() {
+        Button button = (Button) findViewById(R.id.bt_lg);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (et_id.getText().toString().equals(input_id) && et_pw.getText().toString().equals(input_password) ) {
-
-
-                    Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
-                    startActivity(intent);
-                }else {
-                    Toast.makeText(MainActivity.this, "틀림", Toast.LENGTH_SHORT).show();
-                }
-
-
+                Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
+                startActivity(intent);
             }
         });
-    }
+
+
+
+
+        }
 }
